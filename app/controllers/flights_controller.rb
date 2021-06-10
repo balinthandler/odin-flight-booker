115c9b24522f61de6fac61d85d734711ac595701
Balint Handler <handlerbalint@gmail.com>
2021-06-10 16:52:04 +0200

    added date dropdown

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,5 +7,6 @@ class FlightsController < ApplicationController
     @flight = Flight.new
     @flights = Flight.all
     @airports = Airport.all.map { |a| [a.code, a.id] }
+    @dates = Flight.all.map { |f| [f.start_datetime.strftime("%Y/%m/%d"), f.start_datetime] }
   end
 end
